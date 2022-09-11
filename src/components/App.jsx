4c6dbcbfddda4b77223e16aa96fb8d8f9043dcc4
Musanna <musannaal@gmail.com>
2022-09-11 16:52:39 +0600

    homepage setup

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+}
+
+export default App;
