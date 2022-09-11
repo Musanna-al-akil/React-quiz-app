c19222ff651ae3c7a42ccfd39bde3d20b5203d0f
Musanna <musannaal@gmail.com>
2022-09-11 20:45:40 +0600

    result page

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -2,7 +2,7 @@ import classes from "../styles/Answers.module.css";
 import CheckBox from "./CheckBox";
 export default function Answers() {
   return (
-    <div className={classes.Answers}>
+    <div className={classes.answers}>
       <CheckBox className={classes.answer}  text="test answer"/>
     </div>
   );
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import "../styles/App.css";
 import Layout from "./Layout";
-import Login from "./pages/Login";
-import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+//import Login from "./pages/Login";
+//import Quiz from "./pages/Quiz";
 //import Home from "./pages/Home";
 //import Signup from "./pages/Signup";
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Result />
     </Layout>
   );
 }
